a4677b64f8997b2684c0cfea37c2fa9721bbf741
thunduaga <chriswumbojumbo@msn.com>
2015-02-05 09:32:13 -0800

    more stuff.. im so bad at this like holy crap. Everyone talks about how hard this class is but ontop of that i feel like im at an even greater disadvantage. Just got to keep going i guess
    9:32am
    Signed-off-by:thunduaga <chriswumbojumbo@msn.com>

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -38,6 +38,8 @@ class Order extends Application {
         $this->data['pagebody'] = 'show_menu';
         $this->data['order_num'] = $order_num;
         //FIXME
+        $order = $this->orders->get($order_num);
+        $this->data['title'] = $order_num;
 
         // Make the columns
         $this->data['meals'] = $this->make_column('m');
@@ -72,7 +74,7 @@ class Order extends Application {
     // make a menu ordering column
     function make_column($category) {
         //FIXME
-        return $items;
+        return $this->menu->some('category',$category);
     }
 
     // add an item to an order
